7f1ff7ecbf08f0ed521f5a57bf06968a783e486e
NicolasPires <nicolask.pires@gmail.com>
2021-03-21 15:47:18 -0300

    Project created

---
+++ src/main/java/com/nksolucoes/apiorder/ApiOrderApplication.java
@@ -0,0 +1,13 @@
+package com.nksolucoes.apiorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOrderApplication.class, args);
+	}
+
+}
