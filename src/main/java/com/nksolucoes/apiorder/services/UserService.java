6dbe2fff30f605fea5fe43533c1b291c370674b5
NicolasPires <nicolask.pires@gmail.com>
2021-03-21 21:51:37 -0300

    Service layer, component registration

---
+++ src/main/java/com/nksolucoes/apiorder/services/UserService.java
@@ -0,0 +1,27 @@
+package com.nksolucoes.apiorder.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nksolucoes.apiorder.entities.User;
+import com.nksolucoes.apiorder.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
