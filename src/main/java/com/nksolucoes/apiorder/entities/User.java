12005706edae5e7d2aaf6b351eaa3a7c2a363155
NicolasPires <nicolask.pires@gmail.com>
2021-03-21 22:07:36 -0300

    Order, Instant, ISO 8601 part 1

--- src/main/java/com/nksolucoes/apiorder/entities/User.java
+++ src/main/java/com/nksolucoes/apiorder/entities/User.java
@@ -1,13 +1,18 @@
 package com.nksolucoes.apiorder.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -18,6 +23,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -70,6 +78,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -95,8 +107,6 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 	
 	
 }
