8494f0b59009d0e4f76b9dfeeb0590f8b004a74b
NicolasPires <nicolask.pires@gmail.com>
2021-03-22 22:28:12 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/nksolucoes/apiorder/entities/Category.java
+++ src/main/java/com/nksolucoes/apiorder/entities/Category.java
@@ -8,8 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -21,7 +21,7 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
--- src/main/java/com/nksolucoes/apiorder/entities/Product.java
+++ src/main/java/com/nksolucoes/apiorder/entities/Product.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -17,20 +19,21 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
+
 	@JsonIgnore
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
 	}
 
@@ -82,7 +85,7 @@ public class Product implements Serializable {
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-	
+
 	public Set<Category> getCategories() {
 		return categories;
 	}
@@ -111,6 +114,5 @@ public class Product implements Serializable {
 			return false;
 		return true;
 	}
-
 
 }
