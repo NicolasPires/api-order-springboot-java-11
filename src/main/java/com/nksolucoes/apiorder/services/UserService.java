bc45cbf35e1302f6d1c63ac8ab1192240cfccfd7
NicolasPires <nicolask.pires@gmail.com>
2021-03-24 09:12:33 -0300

    Exception handling - delete

--- src/main/java/com/nksolucoes/apiorder/services/UserService.java
+++ src/main/java/com/nksolucoes/apiorder/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.nksolucoes.apiorder.entities.User;
 import com.nksolucoes.apiorder.repositories.UserRepository;
+import com.nksolucoes.apiorder.services.exceptions.DataBaseException;
 import com.nksolucoes.apiorder.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {			
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
